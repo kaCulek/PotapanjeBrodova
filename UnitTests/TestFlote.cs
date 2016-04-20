470f5ecfb1d72786f6a76da12fe70add7cbdba57
kaCulek <karlo2316@gmail.com>
2016-04-20 21:01:48 +0200

    Dodati testove za flotu

--- UnitTests/TestFlote.cs
+++ UnitTests/TestFlote.cs
@@ -32,5 +32,58 @@ namespace UnitTests
             Assert.IsTrue(f.Brodovi.Contains(b2));
             Assert.IsTrue(f.Brodovi.Contains(b3));
         }
+
+        [TestMethod]
+        public void Flota_GađajVraćaPromašajZaPoljeKojeNijeUNitiJednomBrodu()
+        {
+            Flota f = new Flota();
+            Brod b1 = new Brod(SložiPolja(Smjer.Horizontalno, new Polje(0, 0), 5));
+            f.DodajBrod(b1);
+            Brod b2 = new Brod(SložiPolja(Smjer.Vertikalno, new Polje(1, 3), 4));
+            f.DodajBrod(b2);
+
+            Assert.AreEqual(RezultatGađanja.Promašaj, f.Gađaj(new Polje(9, 9)));
+        }
+
+        [TestMethod]
+        public void Flota_GađajVraćaPogodakZaPoljaKojaSuUBrodu()
+        {
+            Flota f = new Flota();
+            Brod b1 = new Brod(SložiPolja(Smjer.Horizontalno, new Polje(0, 0), 5));
+            f.DodajBrod(b1);
+            Brod b2 = new Brod(SložiPolja(Smjer.Vertikalno, new Polje(1, 3), 4));
+            f.DodajBrod(b2);
+
+            Assert.AreEqual(RezultatGađanja.Pogodak, f.Gađaj(new Polje(0, 1)));
+            Assert.AreEqual(RezultatGađanja.Pogodak, f.Gađaj(new Polje(1, 3)));
+        }
+
+        [TestMethod]
+        public void Flota_GađajVraćaPotonućeZaZadnjePogođenoPoljePrvogBroda()
+        {
+            Flota f = new Flota();
+            Brod b1 = new Brod(SložiPolja(Smjer.Horizontalno, new Polje(0, 0), 3));
+            f.DodajBrod(b1);
+            Brod b2 = new Brod(SložiPolja(Smjer.Vertikalno, new Polje(1, 3), 4));
+            f.DodajBrod(b2);
+
+            Assert.AreEqual(RezultatGađanja.Pogodak, f.Gađaj(new Polje(0, 1)));
+            Assert.AreEqual(RezultatGađanja.Pogodak, f.Gađaj(new Polje(0, 2)));
+            Assert.AreEqual(RezultatGađanja.Promašaj, f.Gađaj(new Polje(0, 3)));
+            Assert.AreEqual(RezultatGađanja.Potonuće, f.Gađaj(new Polje(0, 0)));
+        }
+
+        [TestMethod]
+        public void Flota_GađajVraćaPotonućeZaZadnjePogođenoPoljeDrugogBroda()
+        {
+            Flota f = new Flota();
+            Brod b1 = new Brod(SložiPolja(Smjer.Horizontalno, new Polje(0, 0), 3));
+            f.DodajBrod(b1);
+            Brod b2 = new Brod(SložiPolja(Smjer.Vertikalno, new Polje(1, 3), 2));
+            f.DodajBrod(b2);
+
+            Assert.AreEqual(RezultatGađanja.Pogodak, f.Gađaj(new Polje(1, 3)));
+            Assert.AreEqual(RezultatGađanja.Potonuće, f.Gađaj(new Polje(2, 3)));
+        }
     }
 }
