e900ca9ca6d480e8eaaac3df0d3e7f1bf8aed3f5
kaCulek <karlo2316@gmail.com>
2016-05-04 21:30:21 +0200

    Dodaj KružnogPucača i njegov test.

---
+++ PotapanjeBrodova/KružniPucač.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PotapanjeBrodova
+{
+    public class KružniPucač : iPucač
+    {
+        public KružniPucač(Polje prvoPogođeno, Mreža mreža)
+        {
+            this.prvoPogođeno = prvoPogođeno;
+            this.mreža = mreža;
+        }
+        public Polje UputiPucanj()
+        {
+            throw new NotImplementedException();
+        }
+
+        Polje prvoPogođeno;
+        Mreža mreža;
+    }
+}
